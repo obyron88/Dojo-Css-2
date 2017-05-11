c309b2aec771f2e1c7e71a2f14926f379b9667d8
obyron88 <CODA2017@yahoo.com>
2017-05-11 09:20:34 +0200

    php intégré

---
+++ page2.php
@@ -0,0 +1,11 @@
+<?php
+include ("header.html");
+ ?>
+    <section class="one">
+      <h1>OUR STORY & MISION</h1>
+      <img src="./img/feild.jpg"></img>
+      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."Co-Founders Eileen & James Ray</p>
+   </section>
+   <?php
+   include ("footer.html");
+    ?>
---
+++ page3.php
@@ -0,0 +1,18 @@
+<?php
+include ("header.html");
+ ?>
+    <section class="one">
+      <h1 id="meet">TAGGED "GOATS"</h1>
+      <hr id="tc">
+      <img src="./img/sun.jpg"></img>
+      <p id="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div id="exo">
+      <li>2015-8</li>
+      <li>FARMINGHOWTO</li>
+      <li>GOATS</li>
+      </div>
+      <h3>READ MORE-></h3>
+   </section>
+   <?php
+   include ("footer.html");
+    ?>
